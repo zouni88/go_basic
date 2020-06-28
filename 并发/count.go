628499945e0406f30222bb29ec2c5acd642a5fda
cao <cao_cgq@home.com>
2020-06-29 00:07:43 +0800

    modify

---
+++ 并发/count.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Person struct {
+	Money int
+}
+
+func A(msgs chan *Person,name string)  {
+	per := <- msgs
+	per.Money--
+	fmt.Printf("name is %v money==%v\n",name,per.Money)
+	//pe := *per
+	msgs <- per
+
+}
+
+func delay(msgs chan *Person,per *Person)  {
+	time.Sleep(time.Second *2)
+	fmt.Println("。。。")
+	msgs <- per
+	fmt.Println("。。。")
+	fmt.Println(msgs)
+
+}
+
+func main() {
+	person := Person{Money: 10}
+	msgs := make(chan *Person)
+	go A(msgs,"A")
+	go A(msgs,"B")
+	delay(msgs,&person)
+	msgs <- &person
+	fmt.Println(person.Money)
+}
---
+++ 并发/定时器.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	t := time.NewTicker(time.Second)
+	for {
+		times := <- t.C
+		fmt.Println("...",times)
+	}
+
+}
