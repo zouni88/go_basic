deb034a826473c53d7ef35e30a4766bc65e483dc
曹国强 <cao_cgq@163.com>
2020-06-30 11:50:21 +0800

    modify

---
+++ 基础语法/类型转换.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func AnyFunc(v interface{})  {
+
+}
+
+func main() {
+	var a interface{}
+	var c *int
+	//if x,y := c.(*int);y{
+	//	a = *c
+	//}
+	a = c
+
+	fmt.Println()
+	fmt.Println(a)
+}
